8ad1cf4d8e383ce46176a5dbfee94ecc12d53956
Baotong Wu <103976141+Bao-tong@users.noreply.github.com>
2026-04-18 11:55:57 +0100

    Add files via upload

---
+++ Codes/MOPSO-DPRT/Leaderparticle1.m
@@ -0,0 +1,11 @@
+function Position = Leaderparticle(pop,i,nPop)
+
+f1=pop(i).Explore(1);
+f2=i;
+f3=pop(i).Explore(2);
+
+    IPosition=LeaderFindPosition1(pop,f1,f2,f3);
+    Position=IPosition;
+
+
+end
---
+++ Codes/MOPSO-DPRT/SelectLeader.m
@@ -0,0 +1,30 @@
+
+
+function leader = SelectLeader(rep, beta)
+
+    GI = [rep.GridIndex];
+    
+    OC = unique(GI);
+    
+    N = zeros(size(OC));
+    
+    for k = 1:numel(OC)
+        N(k) = numel(find(GI == OC(k)));
+    end
+    
+    P = exp(-beta*N);
+    P = P/sum(P);
+    
+    sci = RouletteWheelSelection(P);
+    
+    sc = OC(sci);
+    
+    SCM = find(GI == sc);
+    
+    smi = randi([1 numel(SCM)]);
+    
+    sm = SCM(smi);
+    
+    leader = rep(sm);
+
+end
